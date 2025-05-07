828dfe49f7a19ba8cdda5fb30c07f9109f643315
WaltDev29 <dwdr29@gmail.com>
2025-05-07 15:21:13 +0900

    push 확인

--- WindowsFormsApp0507_2/Form1.cs
+++ WindowsFormsApp0507_2/Form1.cs
@@ -31,7 +31,6 @@ namespace WindowsFormsApp0507_2
             chart1.ChartAreas[0].RecalculateAxesScale();
         }
         Random rand = new Random();
-
         private void timer1_Tick(object sender, EventArgs e)
         {
             double randNum = rand.NextDouble() * 10.0;
